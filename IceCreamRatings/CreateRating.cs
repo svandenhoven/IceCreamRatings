dcdd35d955731b8c4e040bbf0cda1a6c547908a0
Sander van den Hoven <sandervd@microsoft.com>
2021-11-17 08:54:09 +0000

    changed .netversion in yml

--- IceCreamRatings/CreateRating.cs
+++ IceCreamRatings/CreateRating.cs
@@ -25,16 +25,6 @@ namespace IceCreamRatings
         {
             log.LogInformation("A new rating has been created");
 
-            ///
-//            {
-//                "userId": "cc20a6fb-a91f-4192-874d-132493685376",
-                //  "productId": "4c25613a-a3c2-4ef3-8e02-9c335eb23204",
-                //  "locationName": "Sample ice cream shop",
-                //  "rating": 5,
-                //  "userNotes": "I love the subtle notes of orange in this ice cream!"
-                //}
-
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
 
